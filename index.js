5e0d6cc7df68cd9a784cdbc88886b089c2b1d27e
Saar Yahalom <syahalom@gmail.com>
2012-07-30 00:49:19 +0300

    translate most of goatfish

---
+++ index.js
@@ -0,0 +1,287 @@
+var sqlite3 = require('sqlite3');
+var uuid = require('uuid');
+var sets = require('simplesets');
+var util = require('util');
+
+var findMin = function(list, iterator) {
+  var m = Number.MAX_VALUE;
+  var minItem;
+
+  iterator = iterator || function(n) { return n; };
+
+  list.forEach(function(item) {
+    var num = iterator(item);
+    if (num !== Number.NaN && num < m) { 
+      m = num;
+      minItem = item;
+    }
+  });
+
+  return minItem;
+}
+
+var JsonDB = function(object) {
+  this._serializer = JSON;
+  this.indexes = {};
+  this.connection = null;
+
+  var self = this;
+
+  // Get the names for the index tables in this model.
+  
+  // @returns
+  // {index_name: [index,]} - A tuple of table and field names.
+  function _get_index_table_names(type) {
+    var indexTables = {};
+    self.indexes[type].forEach(function(index) {
+      var tableName = type + '_';
+      tableName += index.join('_');
+      indexTables[tableName] = index;
+    });
+
+    return indexTables;
+  }
+
+  // Return the largest index that can serve a query on these fields.
+  function _get_largest_index(type, fields) {
+    // Turn the attributes and indexes into sets.
+    var field_set = new sets.Set(fields);
+    var indexes = self.indexes[type].map(function(index) { return [new sets.Set(index), index];});
+
+    // Compare each index set to the field set only if it contains all parameters (super set).
+    // XXX: If the database can use partial indexes, we might not need to only
+    // select subsets of the parameters here.
+
+    var min = findMin(indexes, function(index) {
+      if (field_set.issuperset(index[0])) {
+        return field_set.difference(index[0]).length;
+      }
+
+      return Number.NaN;      
+    });
+
+    if (!min) {
+      return [];
+    }
+
+    // Return the index that has the most fields in common with the query.
+    return min[1]
+  }
+
+  function _get_cursor() {
+    if (!self.connection) throw new Error('Cannot proceed without a database connection.');
+
+    return self.connection.cursor()
+  }
+
+  // Create an object from the values retrieved from the database.
+  function _unmarshal(attributes) {
+    return attributes;
+  }
+
+  self.find_one = function(type, parameters) {
+    // Return just one item.
+    var generator = cls.find(parameters);
+
+    try:
+        item = generator.next()
+    except StopIteration:
+        item = None
+    return item
+  } 
+
+  self.find = function(type, parameters) {
+    type = type.toLowerCase();
+
+    // Query the database.
+    var cursor = _get_cursor();
+
+    // If we can use an index, we will offload some of the fields to query
+    // to the database, and handle the rest here.
+    parameters = parameters || {};
+    var index = [];
+
+    if (parameters.id) {
+      index = ['id'];
+    }
+    else {
+      index = _get_largest_index(type, Object.keys(parameters));
+    }
+
+    var table_name = type;
+    var statement;
+
+    if (!index) {
+      // Look through every row.
+      statement = util.format('SELECT * FROM %s;', table_name);
+      cursor.execute(statement);
+    }
+    else if (index.length === 1 && index[0] === 'id') {
+      // If the object id is in the parameters, use only that, since it's
+      // the fastest thing we can do.
+      statement = util.format('SELECT * FROM %s WHERE uuid=?;', table_name);
+      cursor.execute(statement, parameters.id);
+      delete parameters.id;
+    }
+    else {
+      statement = util.format('SELECT x.id, x.uuid, x.data FROM %s x INNER JOIN %s y ON x.uuid = y.uuid WHERE %s;', 
+        table_name,
+        table_name + '_' + index.join('_'),
+        index.join(' = ? AND ') + ' = ?');
+          
+      cursor.execute(statement, [parameters[value] for value in index])
+
+      // Delete the (now) unnecessary parameters, because the database
+      // made sure they match.
+      for (var field in index) {
+        delete parameters[field];
+      }
+    }
+    
+    for id, uuid, data in cursor:
+        loaded_dict = cls._serializer.loads(data.encode("utf-8"))
+        loaded_dict["id"] = uuid
+
+        if parameters:
+            # If there are fields left to match, match them.
+            if all((loaded_dict.get(field, None) == parameters[field]) for field in parameters):
+                yield cls._unmarshal(loaded_dict)
+        else:
+            # Otherwise, just return the object.
+            yield cls._unmarshal(loaded_dict)
+  };
+  
+  self.initialize = function(type) {
+    // Create the necessary tables in the database.
+    type = type.toLowerCase();
+
+    var cursor = _get_cursor();
+    cursor.execute(util.format('CREATE TABLE IF NOT EXISTS %s ( "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "uuid" TEXT NOT NULL, "data" BLOB NOT NULL);', type));
+    cursor.execute(util.format('CREATE UNIQUE INDEX IF NOT EXISTS "%s_uuid_index" on %s (uuid ASC);', type, type));
+
+    if (!self.indexes[type]) self.indexes[type] = [];
+
+    self.indexes[type].forEach(function(index) {
+      // Create an index table.
+      var table_name = util.format('%s_%s', type,  index.join('_'));
+      var statement = 'CREATE TABLE IF NOT EXISTS %s ( "uuid" TEXT NOT NULL '+ table_name;
+      index.forEach(function(field) {
+        statement +=', "' + field + '" TEXT';
+      });
+      statement += ')';
+
+      cursor.execute(statement);
+
+      // Create the index table index.
+      var fields = index.join(' ASC, ');
+      statement = util.format('CREATE INDEX IF NOT EXISTS "%s_index" on %s (%s ASC)',table_name, table_name, fields);
+      cursor.execute(statement);
+    });
+  };
+
+  self.commit = function() {
+    // Commit to the database.
+    self.connection.commit()
+  };
+
+  function _populate_index(obj, cursor, table_name, field_names) {
+    // Get the values of the indexed attributes from the current object.
+    var values = [];
+    field_names.forEach(function(field_name) {
+      // Abort if the attribute doesn't exist, we don't need to add it.
+      // We check this way to make sure the attribute doesn't exist and is
+      // set to None.
+        if (!field_name in obj) {
+          return
+        }
+        
+        values.push(obj[field_name]);
+    });
+
+    values.unshift(obj.id);
+
+    var qMarks = field_names.map(function() { return '?, '}).join('');
+    qMarks.substr(0, qMarks.length - 2);
+
+    // Construct the SQL statement.
+    var statement = 'INSERT OR REPLACE INTO %s ("uuid", "%s") VALUES (%s);', table_name, field_names.join('", "'), qMarks);
+
+    cursor.execute(statement, values);
+  }
+
+  this.save = function(obj, type, commit) {
+    type = type || obj.__type;
+    delete obj.__type;
+
+    if (!type) throw new Error('Missing type parameter');
+
+    commit = commit === undefined ? true : commit;
+
+    type = type.toLowerCase();
+
+    // Persist an object to the database.
+    var cursor = _get_cursor();
+
+    var object_id;
+    var statement;
+
+    if (!obj.id) {
+      object_id =  uuid.v4();
+      statement = util.format('INSERT INTO %s ("uuid", "data") VALUES (?, ?)', type);
+      cursor.execute(statement, [object_id, self._serializer.stringify(obj)))
+    }
+    else {
+      // Temporarily delete the id so it doesn't get stored.
+      object_id = obj.id;
+      delete obj.id;
+
+      statement = util.format('UPDATE %s SET "data" = ? WHERE "uuid" = ?', type);
+      cursor.execute(statement, [self._serializer.stringify(obj), object_id]);
+    }
+
+    // Restore the id.
+    obj.id = object_id;
+
+    // Insert into all indexes:
+    var indexTables = _get_index_table_names(type);
+    var field_names = [];
+
+    for (table_name in indexTables) {
+      field_names = indexTables[table_name];
+    }
+    
+    _populate_index(cursor, table_name, field_names);
+
+    if (commit) self.commit();
+  };
+    
+  self.delete = function(obj, type, commit) {
+    type = type || obj.__type;
+    delete obj.__type;
+
+    if (!type) throw new Error('Missing type parameter');
+
+    commit = commit === undefined ? true : commit;
+
+    type = type.toLowerCase();
+
+    // Delete an object from the database.
+    var cursor = _get_cursor();
+
+    // Get the name of the main table.
+    var table_names = [type];
+
+    // The names of all the index tables.
+    var indexTables = Object.keys(_get_index_table_names(type));
+
+    indexTables.forEach(function(indexTable) { table_names.push(indexTable); });
+
+    // And delete the rows from all of them.
+    table_names.forEach(function(table_name) {
+      var statement = util.format('DELETE FROM %s WHERE "uuid" == ?', table_name);
+      cursor.execute(statement, [self.id]);
+    });
+
+    if (commit) self.commit();
+  };
+};
